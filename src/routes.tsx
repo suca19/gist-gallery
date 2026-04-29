ee0d262f9bd0dc759fc29c8669593c2f30f2f2d3
Carlos <carlos.enrique.suca@gmail.com>
2026-04-29 10:47:23 +0100

    Switch to HashRouter for Github Pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
 import Layout from './app/Layout'
 import Home from './pages/Home'
 import GistDetail from './pages/GistDetail'
+import { createHashRouter } from 'react-router-dom'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([   
   {
     path: '/',
     element: <Layout><Home /></Layout>,
